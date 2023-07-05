c21ce20c7302f4745d14576e1b227c82582b151e
perlpark <hello@jinjoo.me>
2023-07-05 17:57:19 +0900

    feat: 라인 차트 최소/최대 포인트 추가 기능

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,18 @@ function rand(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function createData() {
+function createData(hasMinMax: boolean) {
   const newData = Array.from({ length: 7 }, () => rand(0, 300));
   return {
     data: newData,
     type: 'line',
     markPoint: {
-      data: [
-        { type: 'max', name: 'Max' },
-        { type: 'min', name: 'Min' },
-      ],
+      data: hasMinMax
+        ? [
+            { type: 'max', name: 'Max' },
+            { type: 'min', name: 'Min' },
+          ]
+        : [],
     },
   };
 }
@@ -24,16 +26,17 @@ function createData() {
 export default function Home() {
   const [visualMap, setVisualMap] = useState(false);
   const [markArea, setMarkArea] = useState(false);
-  const [dummyData, setDummyData] = useState([createData()]);
+  const [minMax, setMinMax] = useState(false);
+  const [dummyData, setDummyData] = useState([createData(false)]);
 
   const addData = () => {
-    setDummyData((v) => [...v, createData()]);
+    setDummyData((v) => [...v, createData(minMax)]);
   };
 
   return (
     <div className="w-full h-full">
       <h2 className=" text-2xl font-semibold mb-3">Options</h2>
-      <ul className="flex gap-4 [&_button]:w-40 mb-4">
+      <ul className="flex gap-4 [&_button]:w-44 mb-4">
         <li>
           <button
             type="button"
@@ -63,6 +66,17 @@ export default function Home() {
             className="bg-slate-600 text-white h-10 shadow rounded"
           >
             <input type="checkbox" checked={markArea} /> 영역 강조 추가
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            onClick={() => {
+              setMinMax((v) => !v);
+            }}
+            className="bg-slate-600 text-white h-10 shadow rounded"
+          >
+            <input type="checkbox" checked={minMax} /> 최소/최대 포인트 추가
           </button>
         </li>
       </ul>
