b580f7eb624ffea6ea2f2246ec2947a6d82fe1ef
perlpark <hello@jinjoo.me>
2023-08-22 21:26:21 +0900

    fix: CustomTooltip 차트 tooltip - tooltipConfig 콜백 수정 (빌드 오류 수정)

--- src/app/(playground)/common/custom-tooltip/page.tsx
+++ src/app/(playground)/common/custom-tooltip/page.tsx
@@ -33,28 +33,35 @@ export default function CustomTooltip() {
     <div className="w-full h-full">
       <LineDatasetChart
         dataset={dataset}
-        tooltip={tooltipConfig((obj) => {
-          const month = obj[0].axisValue;
-          const lastYearRevenue = obj[0].data[2];
-          const thisYearRevenue = obj[1]?.data[2];
+        tooltip={tooltipConfig((params) => {
+          const month = params[0].name;
+          let message = '';
 
-          const rate = 100 - (lastYearRevenue / thisYearRevenue) * 100;
-          const arrow = rate > 0 ? '▲' : rate === 0 ? '-' : '▼';
-          const diff = thisYearRevenue - lastYearRevenue;
+          if (Array.isArray(params[0].data)) {
+            const lastYearRevenue = Number(params[0].data[2]);
+            let thisYearRevenue = 0;
 
-          let message = `지난해 ${month}월 총 매출<br/><strong style="font-weight:600;color:#02FF9A">${lastYearRevenue.toLocaleString(
-            'ko-KR'
-          )}원</strong>`;
+            if (Array.isArray(params[1]?.data)) {
+              thisYearRevenue = Number(params[1].data[2]);
+            }
 
-          if (obj.length > 1) {
-            message =
-              `올해 ${month}월 총 매출<br/><strong style="font-weight:600;color:#02FF9A">${thisYearRevenue.toLocaleString(
-                'ko-KR'
-              )}원</strong><br/><strong style="font-weight:600;color:#FF3B30">${arrow}${diff.toLocaleString(
-                'ko-KR'
-              )}원 ${rate.toFixed(1)}%</strong><br/><br/>` + message;
+            const rate = 100 - (lastYearRevenue / (thisYearRevenue || 1)) * 100;
+            const arrow = rate > 0 ? '▲' : rate === 0 ? '-' : '▼';
+            const diff = thisYearRevenue - lastYearRevenue;
+
+            message = `지난해 ${month}월 총 매출<br/><strong style="font-weight:600;color:#02FF9A">${lastYearRevenue.toLocaleString(
+              'ko-KR'
+            )}원</strong>`;
+
+            if (thisYearRevenue) {
+              message =
+                `올해 ${month}월 총 매출<br/><strong style="font-weight:600;color:#02FF9A">${thisYearRevenue.toLocaleString(
+                  'ko-KR'
+                )}원</strong><br/><strong style="font-weight:600;color:#FF3B30">${arrow}${diff.toLocaleString(
+                  'ko-KR'
+                )}원 ${rate.toFixed(1)}%</strong><br/><br/>` + message;
+            }
           }
-
           return message;
         })}
       />
